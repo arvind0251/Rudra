9eab375d5c58b7cdafb6a34a11293cf2e1ce5a95
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-01-16 15:51:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -66,8 +66,8 @@ PING_IMG_URL = getenv("PING_IMG_URL", "https://graph.org//file/389a372e8ae039320
 PLAYLIST_IMG_URL = "https://graph.org//file/3dfcffd0c218ead96b102.png"
 STATS_IMG_URL = "https://graph.org//file/99a8a9c13bb01f9ac7d98.png"
 
-# ✅ Fix: SOUNDCLOUD_IMG_URL variable added
-SOUNDCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
+# Fixing ImportError
+SOUNDCLOUD_IMG_URL = getenv("SOUNDCLOUD_IMG_URL", "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg")
 
 def time_to_seconds(time):
     stringt = str(time)
